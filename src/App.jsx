470f95a6036676e860da4ac88f39790e5b7e0f6a
IkramMaizi <ikrammaizi1@gmail.com>
2024-12-18 23:44:12 +0100

    add logo

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import HomePage from "./pages/HomePage"
 function App() {
 
   return (
-    <div className="bg-[url('./assets/background.png')] bg-cover bg-bottom w-full min-h-screen px-16 pt-8">
+    <div className="bg-[url('./assets/background.png')] bg-cover bg-bottom w-full min-h-screen px-16 pt-6">
       <HomePage />
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Logo from "../assets/logo.png"
 
 export default function () {
   return (
     <div className="w-full flex justify-between items-center">
-        <div className="logo px-3 py-3 bg-gray-400 text-white">Logo</div>
+        <div className="logo px-3 py-3 text-white">
+          <img src={Logo} alt="" className="h-14" />
+
+        </div>
         <ul className="flex gap-6 ">
             <li>Generator</li>
             <li>Favorites</li>
